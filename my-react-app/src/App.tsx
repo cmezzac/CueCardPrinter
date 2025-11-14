89cb423a9cf85365a9faf83362debfdb3c8f5e48
cmezzaca <christophermezzacappa818@yahoo.ca>
2025-11-13 21:02:01 -0500

    final changes

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -4,7 +4,7 @@ import "./JsonUpload.css";
 export default function App() {
   return (
     <div className="page-center">
-      <div className="cuecard-title">Sofia's Cue Cards</div>
+      <div className="cuecard-title">Sofia's Cute Cards</div>
       <JsonUpload />
     </div>
   );
